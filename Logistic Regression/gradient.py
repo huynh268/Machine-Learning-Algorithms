682e23709cdaf7434c93a4d2f37f8aec00753c82
huynh268 <huynhletien@gmail.com>
2018-01-29 15:53:24 -0600

    Added gradient

---
+++ Logistic Regression/gradient.py
@@ -0,0 +1,8 @@
+import numpy
+import sigmoid
+
+def log_likelihood_gradient(features, target, weights):
+	scores = numpy.dot(features, weights)
+	predictions = sigmoid(scores)
+	error = target - predictions
+	return numpy.dot(features.T, error)
